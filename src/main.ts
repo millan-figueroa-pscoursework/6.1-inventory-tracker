a5507068dbadccd0b03b26cc9d1ff816a4311b8c
millan-figueroa <millan.fig@gmail.com>
2025-10-04 21:08:33 -0700

    Imports all functions from models folder

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import './style.css'
+import './models/Product.ts'
+import './models/PhysicalProduct.ts'
+import './models/DigitalProduct.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Inventory Tracker</h1>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Nothing to see here yet.
     </p>
   </div>
 `
